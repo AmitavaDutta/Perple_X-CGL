aff8803b97b3e9fa0a5332e3f8867956e6510b3e
AmitavaDutta <guyfawkes.amitava20@gmail.com>
2024-10-15 21:37:37 +0530

    script updated

--- 1-Project-Vel/vel-perplex.sh
+++ 1-Project-Vel/vel-perplex.sh
@@ -23,7 +23,9 @@ echo "Your project is ${project_name}.dat"
 echo -e "\n"
 # Set a default value for the thermodynamic data file if the user presses enter
 echo "Default Thermodynamic data file is hp02ver.dat"
-echo "Most commonly used thermodynamic data files are: hp02ver.dat, hp633ver.dat, stx21ver.dat and stx24ver.dat"
+echo "Some available examples of thermodynamic data files are: " #hp02ver.dat, hp633ver.dat, stx21ver.dat and stx24ver.dat
+ls datafiles/hp*
+ls datafiles/stx*
 read -p "Enter the thermodynamic data file name (press Enter for default): " thermo_datafile
 thermo_datafile=${thermo_datafile:-hp02ver.dat}
 
@@ -36,6 +38,8 @@ echo "Thermodynamic data file is $thermo_datafile"
 echo -e "\n"
 # Set a default value for the computational option file if the user presses enter
 echo "Default Computational option file is perplex_option.dat"
+echo "Some available examples of computational option files are: " #hp02ver.dat, hp633ver.dat, stx21ver.dat and stx24ver.dat
+ls datafiles/perplex_option*
 read -p "Enter the Computational option file name (press Enter for default): " optn_file
 optn_file=${optn_file:-perplex_option.dat}
 
@@ -48,7 +52,8 @@ echo "Computational option file is $optn_file"
 echo -e "\n"
 # Set a default value for the solution model file if the user presses enter
 echo "Default Solution model file is solution_model.dat"
-echo "Most commonly used Solution model files are: solution_model.dat, stx21_solution_model.dat and stx24_solution_model.dat"
+echo "Available Solution model files are: " #solution_model.dat, stx21_solution_model.dat and stx24_solution_model.dat
+ls datafiles/*solution*
 read -p "Enter the solution model file name (press Enter for default): " soln_mod
 soln_mod=${soln_mod:-solution_model.dat}
 
@@ -61,9 +66,48 @@ echo "Solution Model is $soln_mod"
 echo -e "\n"
 
 #Composition Name
-read -p "Enter a name of the Composition: " composition_name
+#echo "Currently available Compositions are:"
+#ls comp-input/
+#read -p "Enter a name of the Composition: " composition_name
 # Check if the solution model file exists in the /datafiles directory
-composition_file="$comp_files/$composition_name"
+#composition_file="$comp_files/$composition_name"
+
+#!/bin/bash
+
+# Ask the user whether they want Crust or Mantle
+read -p "Do you want to calculate for Crust or Mantle? " layer_type
+
+# Set the comp_files directory based on the user's choice
+if [ "$layer_type" == "Crust" ]; then
+    comp_files="comp-input/Crust"
+elif [ "$layer_type" == "Mantle" ]; then
+    comp_files="comp-input/Mantle"
+else
+    echo "Invalid choice! Please select either Crust or Mantle."
+    exit 1
+fi
+
+# Extract the prefix from the thermo_datafile
+#thermo_datafile="hp02ver.dat" # Example; replace with actual input
+prefix=$(echo "$thermo_datafile" | sed -E 's/[0-9]+.*//')
+
+# Now, display available compositions based on the extracted prefix and layer
+echo "Currently available Compositions for the selected thermodynamics data file are:"
+ls "$comp_files/$prefix"
+
+# Ask for the composition name
+read -p "Enter a name of the Composition: " composition_name
+
+# Construct the path to the composition file
+composition_file="$comp_files/$prefix/$composition_name"
+
+# Check if the composition file exists
+if [ -f "$composition_file" ]; then
+    echo "Composition file found: $composition_file"
+else
+    echo "Composition file not found: $composition_file"
+fi
+
 
 # Initialize arrays for components and mass amounts
 components=()
@@ -102,9 +146,9 @@ fi
 input_file="build_input.txt"  # For all the input and their functions check https://www.perplex.ethz.ch/perplex_66_seismic_velocity.html#top
 
 {
-    echo "$project_name"                     # Project name
-    echo "$datafiles_dir/$thermo_datafile"   # Thermodynamic data file
-    echo "$datafiles_dir/$optn_file"         # Computational option file
+    echo "$project_name"                       # Project name
+    echo "$datafiles_dir/$thermo_datafile"     # Thermodynamic data file
+    echo "$datafiles_dir/$optn_file"           # Computational option file
     echo "n"                                   # Transform components (Y/N)
     echo "2"                                   # Computational mode (2d grid)
     echo "n"                                   # Calculations with saturated fluids (Y/N)    
@@ -124,30 +168,49 @@ input_file="build_input.txt"  # For all the input and their functions check http
     echo "15"                                  # Minimum P(bar)
     echo "150000"                              # Maximum P(bar)
     echo "y"                                   # Specify component amounts by mass (Y/N)
-
     echo "$mass_amounts"                       # Mass amounts for components
-    echo "n"                                   # Output a print file (Y/N)
-    echo "n"                                   # Exclude pure/endmember phases (Y/N)
+    echo "y"                                   # Output a print file (Y/N)
+    echo "y"                                   # Exclude pure/endmember phases (Y/N)
     echo "n"                                   # Do you want to be prompted for phases (Y/N)? 
-    echo "cz"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
-    echo "afchl"	                       #  Enter names, left justified, 1 per line, press <enter> to finish: 
-    echo "h2oL"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
-    echo "tiGL"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "ne"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "feo"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "na2o"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "k2o"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "cao"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "al2o3"		               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "ab"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "q"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "sil8L"			       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "lc"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "san"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "h2oL"	                       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "tiGL"	                       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo ""
     echo "y"                                   # Include solution models (Y/N)
-    echo "$datafiles_dir/$soln_mod"           # Solution model file
-    echo "O(HP)"                               # Solution model: O
-    echo "Pl(h)"                               # Solution model: Pl
-    echo "Sp(HP)"                              # Solution model: Sp
-    echo "Cpx(HP)"                             # Solution model: Cpx
-    echo "Opx(HP)"                             # Solution model: Opx
-    echo "Gt(HP)"                              # Solution model: Gt
+    echo "$datafiles_dir/$soln_mod"            # Solution model file
+    echo "O(HP)" 			       # Solution Model Phases
+    echo "Sp(HP)"  			       # Solution Model Phases
+    echo "Gt(stx8)"  			       # Solution Model Phases
+    echo "C2_c(jca)"  			       # Solution Model Phases
+    echo "Opx(HP)"  			       # Solution Model Phases
+    echo "Cpx(HP)" 			       # Solution Model Phases
+    echo "Pl(h)" 			       # Solution Model Phases
+    echo "Wad(stx8)" 			       # Solution Model Phases
+    echo "San(TH)"  			       # Solution Model Phases
+    echo "GlTrTsPg" 			       # Solution Model Phases
+    echo "B" 	         		       # Solution Model Phases
+    echo "Chl(HP)" 			       # Solution Model Phases
+    echo "KN-Phen" 			       # Solution Model Phases
+    echo "T" 			               # Solution Model Phases
+    echo "A-phase" 			       # Solution Model Phases
+    echo "Atg" 			               # Solution Model Phases
     echo ""                                    # End of solution model selection
     echo "${project_name}"                     # Calculation title (same as project name)
 } > "$input_file"
 
 
 # Run BUILD using the generated input file
-/home/ajay/projects/Perple_X-7.1.6/src/build < "$input_file"
+build < "$input_file"
 
 # Check if BUILD ran successfully
 if [ $? -eq 0 ]; then
@@ -162,7 +225,7 @@ input_file="vertex_input.txt"
     echo "$project_name"                     # Project name
 } > "$input_file"
 # Run VERTEX using the generated input file
-/home/ajay/projects/Perple_X-7.1.6/src/vertex < "$input_file"
+vertex < "$input_file"
 
 # Check if VERTEX ran successfully
 if [ $? -eq 0 ]; then
@@ -197,6 +260,7 @@ input_file="werami_input.txt"	    # For all the input and their functions check 
     echo "2"                # Operational mode: properties on a 2d grid
     echo "38"               # Select a property: multiple property output
     echo "1"                # Properties of the system
+    echo "y"                # Include fluid in computation of aggregate (or modal) properties (y/n)?
     echo "2"                # Property: Density (kg/m3)
     echo "13"               # Property: P-wave velocity (Vp, km/s)
     echo "14"               # Property: S-wave velocity (Vs, km/s)    
@@ -214,7 +278,7 @@ input_file="werami_input.txt"	    # For all the input and their functions check 
 } > "$input_file"
 
 # Run WERAMI with the input file
-/home/ajay/projects/Perple_X-7.1.6/src/werami < "$input_file"
+werami < "$input_file"
 
 # Final output file with the composition name
 output_file="${project_name}.tab"
--- scripts/vel-perplex.sh
+++ scripts/vel-perplex.sh
@@ -20,9 +20,12 @@ fi
 # Prompt the user for the project name
 read -p "Enter a name for the project (this name shall contain short info about themodynamic datafile and the composition): " project_name
 echo "Your project is ${project_name}.dat"
-echo "\n"
+echo -e "\n"
 # Set a default value for the thermodynamic data file if the user presses enter
 echo "Default Thermodynamic data file is hp02ver.dat"
+echo "Some available examples of thermodynamic data files are: " #hp02ver.dat, hp633ver.dat, stx21ver.dat and stx24ver.dat
+ls datafiles/hp*
+ls datafiles/stx*
 read -p "Enter the thermodynamic data file name (press Enter for default): " thermo_datafile
 thermo_datafile=${thermo_datafile:-hp02ver.dat}
 
@@ -32,9 +35,11 @@ if [ ! -f "$datafiles_dir/$thermo_datafile" ]; then
     exit 1
 fi
 echo "Thermodynamic data file is $thermo_datafile"
-echo "\n"
+echo -e "\n"
 # Set a default value for the computational option file if the user presses enter
 echo "Default Computational option file is perplex_option.dat"
+echo "Some available examples of computational option files are: " #hp02ver.dat, hp633ver.dat, stx21ver.dat and stx24ver.dat
+ls datafiles/perplex_option*
 read -p "Enter the Computational option file name (press Enter for default): " optn_file
 optn_file=${optn_file:-perplex_option.dat}
 
@@ -44,9 +49,11 @@ if [ ! -f "$datafiles_dir/$optn_file" ]; then
     exit 1
 fi
 echo "Computational option file is $optn_file"
-echo "\n"
+echo -e "\n"
 # Set a default value for the solution model file if the user presses enter
 echo "Default Solution model file is solution_model.dat"
+echo "Available Solution model files are: " #solution_model.dat, stx21_solution_model.dat and stx24_solution_model.dat
+ls datafiles/*solution*
 read -p "Enter the solution model file name (press Enter for default): " soln_mod
 soln_mod=${soln_mod:-solution_model.dat}
 
@@ -56,12 +63,51 @@ if [ ! -f "$datafiles_dir/$soln_mod" ]; then
     exit 1
 fi
 echo "Solution Model is $soln_mod"
-echo "\n"
+echo -e "\n"
 
 #Composition Name
-read -p "Enter a name of the Composition: " composition_name
+#echo "Currently available Compositions are:"
+#ls comp-input/
+#read -p "Enter a name of the Composition: " composition_name
 # Check if the solution model file exists in the /datafiles directory
-composition_file="$comp_files/$composition_name"
+#composition_file="$comp_files/$composition_name"
+
+#!/bin/bash
+
+# Ask the user whether they want Crust or Mantle
+read -p "Do you want to calculate for Crust or Mantle? " layer_type
+
+# Set the comp_files directory based on the user's choice
+if [ "$layer_type" == "Crust" ]; then
+    comp_files="comp-input/Crust"
+elif [ "$layer_type" == "Mantle" ]; then
+    comp_files="comp-input/Mantle"
+else
+    echo "Invalid choice! Please select either Crust or Mantle."
+    exit 1
+fi
+
+# Extract the prefix from the thermo_datafile
+#thermo_datafile="hp02ver.dat" # Example; replace with actual input
+prefix=$(echo "$thermo_datafile" | sed -E 's/[0-9]+.*//')
+
+# Now, display available compositions based on the extracted prefix and layer
+echo "Currently available Compositions for the selected thermodynamics data file are:"
+ls "$comp_files/$prefix"
+
+# Ask for the composition name
+read -p "Enter a name of the Composition: " composition_name
+
+# Construct the path to the composition file
+composition_file="$comp_files/$prefix/$composition_name"
+
+# Check if the composition file exists
+if [ -f "$composition_file" ]; then
+    echo "Composition file found: $composition_file"
+else
+    echo "Composition file not found: $composition_file"
+fi
+
 
 # Initialize arrays for components and mass amounts
 components=()
@@ -100,11 +146,12 @@ fi
 input_file="build_input.txt"  # For all the input and their functions check https://www.perplex.ethz.ch/perplex_66_seismic_velocity.html#top
 
 {
-    echo "$project_name"                     # Project name
-    echo "$datafiles_dir/$thermo_datafile"   # Thermodynamic data file
-    echo "$datafiles_dir/$optn_file"         # Computational option file
+    echo "$project_name"                       # Project name
+    echo "$datafiles_dir/$thermo_datafile"     # Thermodynamic data file
+    echo "$datafiles_dir/$optn_file"           # Computational option file
     echo "n"                                   # Transform components (Y/N)
     echo "2"                                   # Computational mode (2d grid)
+    echo "n"                                   # Calculations with saturated fluids (Y/N)    
     echo "n"                                   # Calculations with saturated components (Y/N)
     echo "n"                                   # Use chemical potentials, activities, fugacities (Y/N)
 
@@ -121,18 +168,42 @@ input_file="build_input.txt"  # For all the input and their functions check http
     echo "15"                                  # Minimum P(bar)
     echo "150000"                              # Maximum P(bar)
     echo "y"                                   # Specify component amounts by mass (Y/N)
-
     echo "$mass_amounts"                       # Mass amounts for components
-    echo "n"                                   # Output a print file (Y/N)
-    echo "n"                                   # Exclude pure/endmember phases (Y/N)
+    echo "y"                                   # Output a print file (Y/N)
+    echo "y"                                   # Exclude pure/endmember phases (Y/N)
+    echo "n"                                   # Do you want to be prompted for phases (Y/N)? 
+    echo "ne"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "feo"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "na2o"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "k2o"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "cao"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "al2o3"		               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo "ab"				       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "q"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "sil8L"			       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "lc"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "san"	                               #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "h2oL"	                       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    #echo "tiGL"	                       #  Enter names, left justified, 1 per line, press <enter> to finish: 
+    echo ""
     echo "y"                                   # Include solution models (Y/N)
-    echo "$datafiles_dir/$soln_mod"           # Solution model file
-    echo "O(HP)"                               # Solution model: O
-    echo "Pl(h)"                               # Solution model: Pl
-    echo "Sp(HP)"                              # Solution model: Sp
-    echo "Cpx(HP)"                             # Solution model: Cpx
-    echo "Opx(HP)"                             # Solution model: Opx
-    echo "Gt(HP)"                              # Solution model: Gt
+    echo "$datafiles_dir/$soln_mod"            # Solution model file
+    echo "O(HP)" 			       # Solution Model Phases
+    echo "Sp(HP)"  			       # Solution Model Phases
+    echo "Gt(stx8)"  			       # Solution Model Phases
+    echo "C2_c(jca)"  			       # Solution Model Phases
+    echo "Opx(HP)"  			       # Solution Model Phases
+    echo "Cpx(HP)" 			       # Solution Model Phases
+    echo "Pl(h)" 			       # Solution Model Phases
+    echo "Wad(stx8)" 			       # Solution Model Phases
+    echo "San(TH)"  			       # Solution Model Phases
+    echo "GlTrTsPg" 			       # Solution Model Phases
+    echo "B" 	         		       # Solution Model Phases
+    echo "Chl(HP)" 			       # Solution Model Phases
+    echo "KN-Phen" 			       # Solution Model Phases
+    echo "T" 			               # Solution Model Phases
+    echo "A-phase" 			       # Solution Model Phases
+    echo "Atg" 			               # Solution Model Phases
     echo ""                                    # End of solution model selection
     echo "${project_name}"                     # Calculation title (same as project name)
 } > "$input_file"
@@ -148,10 +219,15 @@ else
     echo "BUILD encountered an error."
 fi
 
-# Run BUILD using the generated input file
+# Create the input file for VERTEX
+input_file="vertex_input.txt" 
+{
+    echo "$project_name"                     # Project name
+} > "$input_file"
+# Run VERTEX using the generated input file
 vertex < "$input_file"
 
-# Check if BUILD ran successfully
+# Check if VERTEX ran successfully
 if [ $? -eq 0 ]; then
     echo "VERTEX ran successfully!"
 else
@@ -178,12 +254,13 @@ P_max=$(echo "$t_p_range" | awk 'NR==1 {print $1}')
 current_date=$(date)
 
 # Create the input file for werami
-input_file="input.txt"	    # For all the input and their functions check https://www.perplex.ethz.ch/perplex_66_seismic_velocity.html#top
+input_file="werami_input.txt"	    # For all the input and their functions check https://www.perplex.ethz.ch/perplex_66_seismic_velocity.html#top
 {
     echo "$project_name"    # Project name
     echo "2"                # Operational mode: properties on a 2d grid
     echo "38"               # Select a property: multiple property output
     echo "1"                # Properties of the system
+    echo "y"                # Include fluid in computation of aggregate (or modal) properties (y/n)?
     echo "2"                # Property: Density (kg/m3)
     echo "13"               # Property: P-wave velocity (Vp, km/s)
     echo "14"               # Property: S-wave velocity (Vs, km/s)    
@@ -229,11 +306,13 @@ cp "$output_file" "$project_dir"
 cp "$output_file" "$output_files"
 
 # Move all files with the ${project_name}.* pattern to the project directory
-mv "${project_name}"* "$project_dir"
+mv "${project_name}."* "$project_dir"
+mv "${project_name}_"* "$project_dir"
 
 # Clean up: remove temporary files
-rm "${project_name}_1.tab" "$werami_input" "$input_file"
-rm build_input.txt
+#rm "${project_name}_1.tab" "$werami_input" "$input_file"
+rm build_input.txt vertex_input.txt werami_input.txt
+
 
 echo "Output has been written to the file: ${output_file} and copied to $output_dir"
 
